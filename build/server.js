5354f010d6b39d96635978b1afa5d6bf8744e06f
Guilherme Salum @ Five Clients <gui@fivecreative.com.au>
2019-11-22 01:02:48 +1100

    9

---
+++ build/server.js
@@ -0,0 +1,74 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _helmet = require('helmet');
+
+var _helmet2 = _interopRequireDefault(_helmet);
+
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+var _controller = require('./src/routes/events/controller');
+
+var _controller2 = _interopRequireDefault(_controller);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+var app = (0, _express2.default)();
+var router = _express2.default.Router();
+var server = _http2.default.createServer(app);
+
+app.use(_bodyParser2.default.json({ limit: '10mb' }));
+app.use(_bodyParser2.default.urlencoded({ extended: false }));
+app.use((0, _helmet2.default)());
+app.use((0, _cors2.default)());
+app.set('port', 3010);
+
+router.use('/events', _controller2.default);
+app.use('/', router);
+
+app.get('/', function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res) {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        res.status(200).send("OK");
+
+                    case 1:
+                    case 'end':
+                        return _context.stop();
+                }
+            }
+        }, _callee, undefined);
+    }));
+
+    return function (_x, _x2) {
+        return _ref.apply(this, arguments);
+    };
+}());
+
+server.listen(3010, 'localhost');
+server.on('listening', function () {
+    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+});
+
+exports.default = app;
--- src/routes/events/controller.js
+++ src/routes/events/controller.js
@@ -1,5 +1,4 @@
-import express from 'express';
-import slack from 'slack';
+const express = require('express')
 const eventController = express.Router();
 const token = "xoxp-336669666742-335747632355-832198559011-10d5e3a5ae8296fcad7800b5b9eb20d8";
 
@@ -7,21 +6,13 @@ eventController.get('/', async (req, res) => {
     res.status(200).json({ message: 'OK' })
 });
 
+/**
+ * Receiveing Events
+ */
 eventController.post('/', async (req, res) => { 
-    try {
-        
-        console.log({ 
-        token:  token,
-        channel: req.body.event.channel,
-        text: "Replace this link with useful info!",
-        ts:     req.body.event.message_ts
-    });
-    slack.chat.update({ 
-        token:  token,
-        channel: req.body.event.channel,
-        text: "Replace this link with useful info!",
-        ts:     req.body.event.message_ts
-    });
+    
+    
+    
         if(req.body.type == 'url_verification' && req.body.challenge) {
             res.status(200).send(req.body.challenge);
         } else {
@@ -32,4 +23,4 @@ eventController.post('/', async (req, res) => {
     }
 })
 
-export default eventController;
+module.exports = eventController;
